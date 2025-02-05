c3bd7d7c97235f9e96d048765a688f7083ebd73e
Rivah Ravelomanantsoa <rivahravelomanantsoa@gmail.com>
2025-02-05 16:14:50 +0300

    update

--- front-end-app/src/app/components/sidebar/sidebar.component.ts
+++ front-end-app/src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route?: any;
+}
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatListModule, MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'video_library',
+      label: 'Doctors',
+      route: 'doctors'
+    },
+    {
+      icon: 'analytics',
+      label: 'Patients',
+      route: 'patients'
+    }
 
+  ])
 }
--- front-end-app/src/app/dashboard/dashboard.component.ts
+++ front-end-app/src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { MainContentComponent } from '../components/main-content/main-content.component';
 import { HeaderComponent } from '../components/header/header.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SidebarComponent, MainContentComponent, HeaderComponent],
+  imports: [
+    
+    // MainContentComponent, 
+    // HeaderComponent,
+    MatToolbarModule, MatSidenavModule, SidebarComponent,
+    MatButtonModule,
+    MatIconModule      
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -15,5 +28,6 @@ export class DashboardComponent {
 
   constructor(private router:Router){}
  
-
+  collapsed = signal(false);
+  sidenavwidth = computed(() => this.collapsed() ? '65px': '250px');
 }
