c6af6e84c0924083b5a6b66b6f4194c8f1364021
tahiry nirina <tahirynirina001@gmail.com>
2025-02-05 17:41:54 +0300

    main component

--- front-end-app/src/app/components/main-content/main-content.component.ts
+++ front-end-app/src/app/components/main-content/main-content.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [
+    
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
