70cbd495f10b0faad0233e7601b278fe0dd7782c
Rivah Ravelomanantsoa <rivahravelomanantsoa@gmail.com>
2025-02-05 18:35:09 +0300

    update

--- front-end-app/src/app/dashboard/dashboard.component.ts
+++ front-end-app/src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
 
 
 @Component({
@@ -17,7 +18,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     // MainContentComponent, 
     // HeaderComponent,
     MatToolbarModule, MatSidenavModule, SidebarComponent,
-    MatButtonModule,
+    MatButtonModule, MatCardModule,
     MatIconModule,
     MainContentComponent
 ],
