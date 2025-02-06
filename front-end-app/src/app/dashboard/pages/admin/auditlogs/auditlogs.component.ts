7dcc4d2ecb6280ed46e1fea1648c0e6b16c55316
Rivah Ravelomanantsoa <rivahravelomanantsoa@gmail.com>
2025-02-06 10:47:18 +0300

    update

--- front-end-app/src/app/dashboard/pages/admin/auditlogs/auditlogs.component.ts
+++ front-end-app/src/app/dashboard/pages/admin/auditlogs/auditlogs.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-auditlogs',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule,DatePipe],
   templateUrl: './auditlogs.component.html',
   styleUrl: './auditlogs.component.css'
 })
-export class AuditlogsComponent {
+export class AuditlogsComponent implements OnInit {
 
+  displayedColumns: string[] = ['action', 'user', 'timestamp']
+
+  auditEntries = [
+    { id: 1, action: 'Création du patient', user: 'admin', timestamp: new Date() },
+    { id: 2, action: 'Modification des informations du patient', user: 'doctor1', timestamp: new Date() },
+    { id: 3, action: 'Suppression du patient', user: 'admin', timestamp: new Date() },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+    // Pas d'appel API ici, les données sont statiques
+  }
 }
