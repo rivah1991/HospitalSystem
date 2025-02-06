4288134df3180ba0bc318a4202ddd592c7b1f862
Rivah Ravelomanantsoa <rivahravelomanantsoa@gmail.com>
2025-02-06 10:32:04 +0300

    Update dashboard.component.ts

--- front-end-app/src/app/dashboard/dashboard.component.ts
+++ front-end-app/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, computed, signal } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
-import { HeaderComponent } from '../components/header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -13,11 +12,9 @@ import {MatCardModule} from '@angular/material/card';
   selector: 'app-dashboard',
   standalone: true,
   imports: [ 
-    // HeaderComponent,
     MatToolbarModule, MatSidenavModule, SidebarComponent,
     MatButtonModule, MatCardModule,
     MatIconModule, RouterModule,
-    // MainContentComponent
 ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
