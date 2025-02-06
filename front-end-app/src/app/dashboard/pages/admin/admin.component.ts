4763464a4299a004d75ec44d3d1da9781c8da22f
Rivah Ravelomanantsoa <rivahravelomanantsoa@gmail.com>
2025-02-06 12:09:20 +0300

    update

--- front-end-app/src/app/dashboard/pages/admin/admin.component.ts
+++ front-end-app/src/app/dashboard/pages/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';  
@@ -6,6 +6,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-admin',
@@ -17,36 +18,42 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: string[] = ['name', 'approval', 'role'];  // Colonnes à afficher : nom, approbation et rôle
+  displayedColumns: string[] = ['name', 'role', 'approval'];  // Colonnes à afficher : nom, approbation et rôle
 
   // Exemple d'utilisateurs avec un statut d'attente d'approbation
   users = [
-    { id: 1, name: 'User 1', approved: false, role: 'admin' },
-    { id: 2, name: 'User 2', approved: false, role: 'professionnal' },
-    { id: 3, name: 'User 3', approved: false, role: 'professionnal' },
-    { id: 4, name: 'User 4', approved: true, role: 'admin' },
-    { id: 5, name: 'User 5', approved: false, role: 'professionnal' },
-    { id: 6, name: 'User 6', approved: true, role: 'admin' },
-    { id: 7, name: 'User 7', approved: true, role: 'professionnal' },
-    { id: 8, name: 'User 8', approved: false, role: 'admin' },
-    { id: 9, name: 'User 9', approved: false, role: 'professionnal' },
-    { id: 10, name: 'User 10', approved: true, role: 'admin' }
+    { id: 1, name: 'User 1', role: 'admin', approved: false },
+    { id: 2, name: 'User 2', role: 'professional', approved: false },
+    { id: 3, name: 'User 3', role: 'professional', approved: false },
+    { id: 4, name: 'User 4', role: 'admin', approved: true },
+    { id: 5, name: 'User 5', role: 'professional', approved: false },
+    { id: 6, name: 'User 6', role: 'admin', approved: true },
+    { id: 7, name: 'User 7', role: 'professional', approved: true },
+    { id: 8, name: 'User 8', role: 'admin', approved: false },
+    { id: 9, name: 'User 9', role: 'professional', approved: false },
+    { id: 10, name: 'User 10', role: 'admin', approved: true }
   ];
-
+  
   dataSource = new MatTableDataSource(this.users);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;  // Utilisation du modificateur `!` pour indiquer que cette propriété sera définie plus tard
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   // Méthodes pour changer l'approbation et le rôle
   toggleApproval(user: any): void {
     user.approved = !user.approved;
   }
 
-  toggleRole(user: any): void {
-    user.role = user.role === 'professionnal' ? 'admin' : 'professionnal';
-  }
+  // toggleRole(user: any): void {
+  //   user.role = user.role === 'professional' ? 'admin' : 'professional';
+  // }
 }
--- front-end-app/src/app/dashboard/pages/admin/auditlogs/auditlogs.component.ts
+++ front-end-app/src/app/dashboard/pages/admin/auditlogs/auditlogs.component.ts
@@ -1,27 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { DatePipe } from '@angular/common';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-auditlogs',
   standalone: true,
-  imports: [MatTableModule,DatePipe],
+  imports: [MatTableModule,DatePipe,MatSortModule,MatPaginatorModule],
   templateUrl: './auditlogs.component.html',
   styleUrl: './auditlogs.component.css'
 })
 export class AuditlogsComponent implements OnInit {
+   
+  constructor() {}
 
   displayedColumns: string[] = ['action', 'user', 'timestamp']
 
   auditEntries = [
-    { id: 1, action: 'Création du patient', user: 'admin', timestamp: new Date() },
-    { id: 2, action: 'Modification des informations du patient', user: 'doctor1', timestamp: new Date() },
-    { id: 3, action: 'Suppression du patient', user: 'admin', timestamp: new Date() },
+    { id: 1, action: 'Patient creation', user: 'admin', timestamp: new Date() },
+    { id: 2, action: 'Patient editing', user: 'doctor1', timestamp: new Date() },
+    { id: 3, action: 'Patient Deletion', user: 'admin', timestamp: new Date() },
+    { id: 4, action: 'Patient creation', user: 'admin', timestamp: new Date() },
+    { id: 5, action: 'Patient editing', user: 'doctor1', timestamp: new Date() },
+    { id: 6, action: 'Patient Deletion', user: 'admin', timestamp: new Date() },
+    { id: 7, action: 'Patient creation', user: 'admin', timestamp: new Date() },
+    { id: 8, action: 'Patient editing', user: 'doctor1', timestamp: new Date() },
+    { id: 9, action: 'Patient Deletion', user: 'admin', timestamp: new Date() },
   ];
 
-  constructor() {}
+  dataAudit = new MatTableDataSource(this.auditEntries);
+  
+    @ViewChild(MatPaginator) paginator!: MatPaginator;  // Utilisation du modificateur `!` pour indiquer que cette propriété sera définie plus tard
+  
+    ngAfterViewInit() {
+      this.dataAudit.paginator = this.paginator;
+    }
+ 
 
-  ngOnInit() {
-    // Pas d'appel API ici, les données sont statiques
-  }
+    ngOnInit(): void {}
 }
