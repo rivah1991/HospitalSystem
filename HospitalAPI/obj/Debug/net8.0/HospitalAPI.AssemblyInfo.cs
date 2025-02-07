827aad6f158525bd8fff72c0b90297b429226e04
Rivah Ravelomanantsoa <rivahravelomanantsoa@gmail.com>
2025-02-07 09:45:53 +0300

    update

--- HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
+++ HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9569efe3e2bdef14e3997c3042cfff6c3c1d59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd5f4fb0ccc7d97755106e18dda9bec476d7a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
